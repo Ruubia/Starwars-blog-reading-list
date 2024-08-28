411ddad624c3183b1192e9ce205cb4ded96fb38f
Beatriz Carmona Jurado <b.carmonaj@gmail.com>
2024-08-28 16:38:27 +0100

    Update home.js

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,45 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useContext } from "react";
+import { Card } from "../component/cardCharacters";
+import { CardForPlanets } from "../component/cardPlanets";
+import { CardForVehicles } from "../component/cardVehicles";
+import { Context } from "../store/appContext";
 import "../../styles/home.css";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+    const { store } = useContext(Context);
+
+    const eachCharacter = store.people.map((people, index) => (
+        <Card key={index} people={people} />
+    ));
+
+    const eachPlanet = store.planets.map((planet, index) => (
+        <CardForPlanets key={index} planet={planet} />
+    ));
+
+    const eachVehicle = store.vehicles.map((vehicle, index) => (
+        <CardForVehicles key={index} vehicle={vehicle} />
+    ));
+
+    return (
+        <>
+            <div className="m-5">
+                <h1>Characters</h1>
+                <div className="scrollmenu d-flex">
+                    {eachCharacter}
+                </div>
+            </div>
+            <div className="m-5">
+                <h1>Planets</h1>
+                <div className="scrollmenu d-flex">
+                    {eachPlanet}
+                </div>
+            </div>
+            <div className="m-5">
+                <h1>Vehicles</h1>
+                <div className="scrollmenu d-flex">
+                    {eachVehicle}
+                </div>
+            </div>
+        </>
+    );
+};
