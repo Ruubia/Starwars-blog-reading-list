eda31a9f38fd0c0fea77c068978145d268bed5fb
Beatriz Carmona Jurado <b.carmonaj@gmail.com>
2024-08-28 16:36:19 +0100

    Create vehiclesDetails.js

---
+++ src/js/views/vehiclesDetails.js
@@ -0,0 +1,63 @@
+import React, { useEffect, useState, useContext } from "react";
+import { useParams } from "react-router-dom";
+import { Context } from "../store/appContext";
+
+export const VehiclesDetails = () => {
+    const { store, actions } = useContext(Context);
+    const { id } = useParams();
+    const [details, setDetails] = useState(null);
+
+    useEffect(() => {
+        actions.getVehiclesInfo(id)
+            .then(() => {
+                setDetails(store.vehiclesInfo[id]);
+            })
+            .catch(error => {
+                console.error("Error fetching vehicle details:", error);
+            });
+    }, [id, store.vehiclesInfo, actions]);
+
+    if (!details) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <div className="container m-5">
+            <div className="row">
+                <div className="col-md-4">
+                    <img src={`https://starwars-visualguide.com/assets/img/vehicles/${id}.jpg`} className="img-fluid" alt={details.name} />
+                </div>
+                <div className="col-md-8">
+                    <h1>{details.name}</h1>
+                    <p>A vehicle within the Star Wars universe</p>
+                    <div className="row">
+                        <div className="col-md-4">
+                            <strong>Name:</strong>
+                            <p>{details.name}</p>
+                        </div>
+                        <div className="col-md-4">
+                            <strong>Model:</strong>
+                            <p>{details.model}</p>
+                        </div>
+                        <div className="col-md-4">
+                            <strong>Manufacturer:</strong>
+                            <p>{details.manufacturer}</p>
+                        </div>
+                        <div className="col-md-4">
+                            <strong>Cost in Credits:</strong>
+                            <p>{details.cost_in_credits}</p>
+                        </div>
+                        <div className="col-md-4">
+                            <strong>Length:</strong>
+                            <p>{details.length}</p>
+                        </div>
+                        <div className="col-md-4">
+                            <strong>Max Atmosphering Speed:</strong>
+                            <p>{details.max_atmosphering_speed}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
